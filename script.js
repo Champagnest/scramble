67812a886fbddec4437a3851f5329c20fbdb2c88
Champagnest <mchampagned@protonmail.com>
2022-04-16 13:45:55 -0700

    can generate password but need to make it pass through prompts

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Assignment code here
+var password=document.getElementById("password");
+
+window.confirm("choose a password between 8-128 characters")
+window.confirm("include lowercase, uppercase, special chars?")
+
+
+function genPassword() {
+  var length = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120]
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}  
 
 
 // Get references to the #generate element
@@ -6,7 +21,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = genPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
